9dfc71bac384d6e0bdbe5b54db5b19be3940382a
aleksi2105 <aleksi2105@yandex.ru>
2026-05-10 03:50:40 +0300

    add: attemps

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 function startGame() {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
+  let attempts = 10;
   function question() {
     const userInput = prompt("Угадай число от 1 до 100");
 
@@ -21,19 +22,37 @@ function startGame() {
       return;
     }
 
+    attempts--;
+
     if (userNumber < randomNumber) {
-      alert("Загаданное число больше");
+      alert(`Загаданное число больше, осталось попыток: ${attempts}`);
+      if (attempts === 0) {
+        if (confirm("Попытки закончились, хотите сыграть еще?")) {
+          startGame();
+        }
+        return;
+      }
       question();
       return;
     }
     if (userNumber > randomNumber) {
-      alert("Загаданное число меньше");
+      alert(`Загаданное число меньше, осталось попыток: ${attempts}`);
+      if (attempts === 0) {
+        if (confirm("Попытки закончились, хотите сыграть еще?")) {
+          startGame();
+        }
+        return;
+      }
       question();
       return;
     }
 
     if (userNumber === randomNumber) {
       alert("Поздравляю, Вы угадали!!!");
+      if (confirm("Хотите сыграть еще?")) {
+        startGame();
+      }
+      return;
     }
   }
   question();
