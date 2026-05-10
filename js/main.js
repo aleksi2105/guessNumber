2ddc007134b423b3c83eabbc25dde4d84f233bca
aleksi2105 <aleksi2105@yandex.ru>
2026-05-10 03:23:57 +0300

    add: game script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+function startGame() {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  function question() {
+    const userInput = prompt("Угадай число от 1 до 100");
+
+    if (userInput === null) {
+      alert("Игра окончена");
+      return;
+    }
+    if (userInput.trim() === "") {
+      alert("Введи число!");
+      question();
+      return;
+    }
+    const userNumber = Number(userInput);
+    if (isNaN(userNumber)) {
+      alert("Введи число!");
+      question();
+      return;
+    }
+
+    if (userNumber < randomNumber) {
+      alert("Загаданное число больше");
+      question();
+      return;
+    }
+    if (userNumber > randomNumber) {
+      alert("Загаданное число меньше");
+      question();
+      return;
+    }
+
+    if (userNumber === randomNumber) {
+      alert("Поздравляю, Вы угадали!!!");
+    }
+  }
+  question();
+}
+startGame();
